da4eee9b4b0897aa143ce26238ce59e2f77c1104
Alexander Kolbasov <akolb@cloudera.com>
2016-12-08 15:04:18 -0800

    Added simple API test and benchmark for role creation

---
+++ sentryapi/api_test.go
@@ -0,0 +1,90 @@
+package sentryapi
+
+import (
+	"flag"
+	"os"
+	"os/user"
+	"strconv"
+	"testing"
+	"fmt"
+)
+
+const (
+	defaultHost = "localhost"
+	defaultPort = "8038"
+)
+
+var client ClientAPI
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+	host := os.Getenv("SENTRY_HOST")
+	port := os.Getenv("SENTRY_PORT")
+	userName := os.Getenv("SENTRY_USER")
+
+	if host == "" {
+		host = defaultHost
+	}
+	if port == "" {
+		port = defaultPort
+	}
+	if userName == "" {
+		currentUser, _ := user.Current()
+		userName = currentUser.Username
+	}
+
+	portVal, err := strconv.Atoi(port)
+	if err != nil {
+		panic("invalid port" + port)
+	}
+	client, err = GetClient(PolicyProtocol, host, portVal, "", userName)
+	if err != nil {
+		panic(err)
+	}
+	os.Exit(m.Run())
+}
+
+func TestSentryClient_CreateRole(t *testing.T) {
+	roleName := "testRole"
+	err := client.CreateRole(roleName)
+	if err != nil {
+		t.Error(err)
+	}
+	err = client.RemoveRole(roleName)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func BenchmarkSentryClient_CreateRole(b *testing.B) {
+	roleName := "testRole"
+	for i := 0; i < b.N; i++ {
+		err := client.CreateRole(roleName)
+		if err != nil {
+			b.Fail()
+		}
+		err = client.RemoveRole(roleName)
+		if err != nil {
+			b.Fail()
+		}
+	}
+}
+
+func BenchmarkSentryClient_CreateRoles(b *testing.B) {
+	roleName := "testRole"
+	for i := 0; i < b.N; i++ {
+		name := fmt.Sprint("%s_%d", roleName, i)
+		err := client.CreateRole(name)
+		if err != nil {
+			b.Fail()
+		}
+	}
+	for i := 0; i < b.N; i++ {
+		name := fmt.Sprint("%s_%d", roleName, i)
+		err := client.RemoveRole(name)
+		if err != nil {
+			continue
+			b.Errorf("can't remove %s: %v", name, err)
+		}
+	}
+}
