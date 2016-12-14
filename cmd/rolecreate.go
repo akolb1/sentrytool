c34c9cbf7d8eb23968dda7f6591e101c68e3b2a9
Alexander Kolbasov <akolb@cloudera.com>
2016-12-14 15:11:35 -0800

    move --noverify flag to role create - really

--- cmd/rolecreate.go
+++ cmd/rolecreate.go
@@ -70,6 +70,6 @@ func roleCreate(cmd *cobra.Command, args []string) {
 }
 
 func init() {
-	roleCmd.Flags().BoolP(noverifyOpt, "", false, "do not verify role existence")
+	roleCreateCmd.Flags().BoolP(noverifyOpt, "", false, "do not verify role existence")
 	roleCmd.AddCommand(roleCreateCmd)
 }
